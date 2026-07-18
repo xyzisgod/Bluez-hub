4a015147e513173688ce3f603e7a0f1903640860
xyzisgod <306354529+xyzisgod@users.noreply.github.com>
2026-07-17 19:50:49 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,9 @@
-local Farm = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzisgod/Bluez-hub/refs/heads/main/Modules/Farm/main.lua"))()
-local Stats = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzisgod/Bluez-hub/refs/heads/main/Modules/Stats/main.lua"))()
-local UI = loadstring(game:HttpGet(""))()
+local ProjectUrl = "https://raw.githubusercontent.com/xyzisgod/Bluez-hub/refs/heads/main/"
 
+function createLink(url)
+	return ProjectUrl .. url
+end
+
+local Farm = loadstring(game:HttpGet(createLink("Modules/Farm/main.lua")))()
+local Stats = loadstring(game:HttpGet(createLink("Modules/Stats/main.lua")))()
+local UI = loadstring(game:HttpGet(createLink("UI.lua")))()
