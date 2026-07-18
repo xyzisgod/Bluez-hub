2fce5c0caeb8217913189db1383c12479f3f9da3
xyzisgod <7ynpgyywzn@privaterelay.appleid.com>
2026-07-17 19:07:46 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,3 @@
 local Farm = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzisgod/Bluez-hub/refs/heads/main/Modules/Farm/main.lua"))()
+local Stats = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzisgod/Bluez-hub/refs/heads/main/Modules/Stats/main.lua"))()
+local UI = loadstring(game:HttpGet(""))()
