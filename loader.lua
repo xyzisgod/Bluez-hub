8bf0065b7dad56fb7a921d52e790ca5888e594f1
xyzisgod <306354529+xyzisgod@users.noreply.github.com>
2026-07-20 04:28:45 -0700

    Fix loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,18 @@
 local ProjectUrl = "https://raw.githubusercontent.com/xyzisgod/Bluez-hub/refs/heads/main/"
 
+print("Commons")
 local Commons = loadstring(game:HttpGet(ProjectUrl .. "commons.lua"))()
 
+print("Farm")
 local FarmModule = loadstring(game:HttpGet(Commons:CreateLink("Modules/Farm/main.lua")))()
 
+print("Stats")
 local StatsModule = loadstring(game:HttpGet(Commons:CreateLink("Modules/Stats/main.lua")))()
 
+print("Quest")
 local QuestModule = loadstring(game:HttpGet(Commons:CreateLink("Modules/Quest/main.lua")))()
 
+print("UI")
 local UI = loadstring(game:HttpGet(ProjectUrl .. "UI.lua"))()
 
 print("Stats")
